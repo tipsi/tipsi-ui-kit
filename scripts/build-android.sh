af3f251cb796bff340b3a8e6699e385c3e378f29
Anton Petrov <petrov.aap@gmail.com>
2017-01-06 20:43:37 +0700

    Fix android build script

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+# Go to android path
+cd android
+# Remove old release keystore
+rm -rf app/release.keystore
 # Generate release keystore
 keytool \
   -v \
   -genkey \
-  -keystore android/app/release.keystore \
+  -keystore app/release.keystore \
   -storepass android \
   -alias androidreleasekey \
   -keypass android \
   -dname 'CN=Android Debug,O=Android,C=US'
-# Go to android path
-cd android
 # Run release build
 ./gradlew assembleRelease --console=plain -S
