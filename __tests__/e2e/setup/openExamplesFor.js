5b2468244dc4d49bedb1af40510aefda6ce2e700
Anton Petrov <petrov.aap@gmail.com>
2017-01-09 22:40:35 +0700

    Implemented back to list action in e2e tests

--- __tests__/e2e/setup/openExamplesFor.js
+++ __tests__/e2e/setup/openExamplesFor.js
@@ -1,15 +1,29 @@
 import helper from 'tipsi-appium-helper'
 
-helper.extend('openExampleFor', async (exampleName, wait = 60000) => {
-  const { driver, idFromAccessId } = helper
+helper.extend('openExampleFor', async (exampleName, wait = 6000) => {
+  const { driver, platform, idFromAccessId, idFromXPath } = helper
 
   const listId = idFromAccessId('List')
-
+  const backId = idFromXPath(`//
+    XCUIElementTypeApplication/XCUIElementTypeWindow/
+    XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/
+    XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/
+    XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/
+    XCUIElementTypeOther
+  `)
   const examplesNameId = idFromAccessId(exampleName)
 
   // TODO: add find item with swipe
 
-  await driver.waitForVisible(listId, wait)
+  try {
+    await driver.waitForVisible(listId, wait)
+  } catch (error) {
+    if (platform('android')) {
+      await driver.back()
+    } else {
+      await driver.click(backId)
+    }
+  }
 
   await driver.waitForVisible(examplesNameId, wait).click(examplesNameId)
 })
