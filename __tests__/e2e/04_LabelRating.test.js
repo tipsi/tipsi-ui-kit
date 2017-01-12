2fd695e7466403a41016f0022d2a464d91e84ad3
Dmytro Zavgorodniy <dmytro.zavgorodniy@gmail.com>
2017-01-12 19:29:08 +0200

    Update e2e tests

--- __tests__/e2e/04_LabelRating.test.js
+++ __tests__/e2e/04_LabelRating.test.js
@@ -8,8 +8,10 @@ test('<LabelRating />', async (t) => {
     ios: idFromXPath(`//
       XCUIElementTypeScrollView/XCUIElementTypeOther/XCUIElementTypeOther
     `),
-    android: idFromXPath(`//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/
-      android.view.View[1]/android.view.View[2]/android.view.View[1]/android.widget.ScrollView[1]/android.view.View[1]/
+    android: idFromXPath(`//
+      android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/
+      android.widget.FrameLayout[1]/android.view.View[1]/android.view.View[2]/
+      android.view.View[1]/android.widget.ScrollView[1]/android.view.View[1]/
       android.view.View
     `),
   })
