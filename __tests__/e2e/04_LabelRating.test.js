e3f49fac7b1a5f37f7fe8c7d7ac8506f5d83a4b2
Dmytro Zavgorodniy <dmytro.zavgorodniy@gmail.com>
2017-01-12 15:02:58 +0200

    Update e2e tests

--- __tests__/e2e/04_LabelRating.test.js
+++ __tests__/e2e/04_LabelRating.test.js
@@ -4,28 +4,51 @@ import helper from 'tipsi-appium-helper'
 const { driver, select, idFromXPath } = helper
 
 test('<LabelRating />', async (t) => {
-  const labelRatingGroupId = select({
+  const groupId = select({
     ios: idFromXPath(`//
-      XCUIElementTypeScrollView/*/*/XCUIElementTypeOther[2]/XCUIElementTypeOther/
-      XCUIElementTypeOther/XCUIElementTypeOther
+      XCUIElementTypeScrollView/XCUIElementTypeOther/XCUIElementTypeOther
     `),
     android: idFromXPath(`//
-      android.widget.ScrollView[1]/android.view.ViewGroup[1]/
-      android.view.ViewGroup[1]/android.view.ViewGroup
+      android.widget.ScrollView[1]/android.view.ViewGroup[1]/android.view.ViewGroup
     `),
   })
-  const labelRatingItemId = select({
-    ios: idFromXPath(`${labelRatingGroupId}[labelRatingId]/XCUIElementTypeOther/XCUIElementTypeStaticText`),
-    android: idFromXPath(`${labelRatingGroupId}[labelRatingId]/android.view.ViewGroup[1]/android.widget.TextView[1]`),
-  })
-  const sequence = select({ ios: [1, 2, 3], android: [2, 3, 4] })
+
+  const models = [
+    { id: 1, title: 'WS', rating: '92' },
+    { id: 2, title: 'LR' },
+    { id: 3, title: 'NT', rating: '22' },
+    { id: 4, title: 'YY', rating: '31' },
+  ]
 
   try {
     await helper.openExampleFor('<LabelRating />')
 
-    for (const labelRatingId of sequence) {
-      const currentLabelRatingId = labelRatingItemId.replace('labelRatingId', labelRatingId)
-      await driver.waitForVisible(currentLabelRatingId, 20000)
+    for (const model of models) {
+      const id = `${groupId}[${model.id}]`
+
+      const currentTitleId = select({
+        ios: `${id}/XCUIElementTypeOther[2]/XCUIElementTypeOther/XCUIElementTypeOther[1]/XCUIElementTypeStaticText`,
+        android: `${id}/android.view.ViewGroup[2]/android.view.ViewGroup[1]/android.widget.TextView[1]`,
+      })
+
+      await driver.waitForVisible(currentTitleId, 20000)
+      t.pass(`LabelRating (id: ${model.id}) title text should be visible`)
+
+      const title = await driver.getText(currentTitleId)
+      t.equal(title, model.title, `LabelRating (id: ${model.id}) should has title: ${model.title}`)
+
+      if (model.rating) {
+        const currentRatingId = select({
+          ios: `${id}/XCUIElementTypeOther[2]/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeStaticText`,
+          android: `${id}/android.view.ViewGroup[2]/android.view.ViewGroup[2]/android.widget.TextView[1]`,
+        })
+
+        await driver.waitForVisible(currentRatingId, 20000)
+        t.pass(`LabelRating (id: ${model.id}) rating text should be visible`)
+
+        const rating = await driver.getText(currentRatingId)
+        t.equal(rating, model.rating, `LabelRating (id: ${model.id}) should has rating: ${model.rating}`)
+      }
     }
 
     t.pass('<LabelRating /> example should be visible')
