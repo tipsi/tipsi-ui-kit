109dbb4b07f157e1a59722b8268ae9b801c77414
Anton Petrov <petrov.aap@gmail.com>
2017-01-12 18:03:59 +0700

    Updated e2e tests

--- __tests__/e2e/04_Carousel.test.js
+++ __tests__/e2e/04_Carousel.test.js
@@ -1,7 +1,7 @@
 import test from 'tape-async'
 import helper from 'tipsi-appium-helper'
 
-const { driver, platform, select, idFromXPath } = helper
+const { driver, select, idFromXPath } = helper
 
 helper.extend('swipe', async (selector, direction, speed) => {
   const mapDirection = helper.select({
@@ -43,38 +43,15 @@ test('<Carousel />', async (t) => {
       android.view.ViewGroup/android.widget.TextView[2]
     `),
   })
-  const elementsCount = 5
 
   try {
     await helper.openExampleFor('<Carousel />')
 
-    await driver.waitForVisible(carouselId, 30000)
+    await driver
+      .waitForVisible(carouselId, 30000)
+      .waitForVisible(carouselItemId)
 
     t.pass('<Carousel /> example should be visible')
-
-    if (platform('android')) {
-      const firstElementText = await driver
-        .waitForVisible(carouselItemId, 30000)
-        .getText(carouselItemId)
-      await helper.swipe(carouselId, 'left', 2000)
-      const afterSwipeElementText = await driver
-        .waitForVisible(carouselItemId, 30000)
-        .getText(carouselItemId)
-
-      t.notEqual(
-        afterSwipeElementText,
-        firstElementText,
-        '<Carousel /> should be swipeable'
-      )
-    } else {
-      const elementsText = await driver.getText(carouselItemId)
-
-      t.same(
-        elementsText.length,
-        elementsCount,
-        '<Carousel /> should render five items'
-      )
-    }
   } catch (error) {
     await helper.screenshot()
     await helper.source()
