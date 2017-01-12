838a4a2f6c1bd35f743ff2c488be30d552c53caa
Dmytro Zavgorodniy <dmytro.zavgorodniy@gmail.com>
2017-01-12 11:47:45 +0200

    Update example for <LabelRating />

--- uiexplorer/examples/LabelRating.js
+++ uiexplorer/examples/LabelRating.js
@@ -18,32 +18,32 @@ register.addExample({
     title: 'Title Only',
     description: 'Label rating with title',
     render: () => (
-      <LabelRating title="WS" />
+      <LabelRating title="LR" />
     ),
   }, {
     title: 'Custom Style',
     description: 'Label rating with title and rating, custom style',
     render: () => (
-      <LabelRating title="WS" rating="92" style={{ backgroundColor: ColorPallete.GREEN }} />
+      <LabelRating title="NT" rating="22" style={{ backgroundColor: ColorPallete.GREEN }} />
     ),
   }, {
     title: 'Custom Title Style',
     description: 'Label rating with title and rating, custom title style',
     render: () => (
-      <LabelRating title="WS" rating="92" titleStyle={{ fontSize: 22, color: ColorPallete.YELLOW }} />
+      <LabelRating title="YY" rating="31" titleStyle={{ fontSize: 22, color: 'aliceblue' }} />
     ),
   }, {
     title: 'Custom Rating Style',
     description: 'Label rating with title and rating, custom rating style',
     render: () => (
-      <LabelRating title="WS" rating="92" ratingStyle={{ fontSize: 22, color: ColorPallete.BROWN }} />
+      <LabelRating title="ER" rating="99" ratingStyle={{ fontSize: 22, color: ColorPallete.BROWN }} />
     ),
   }, {
     title: 'Multiple LabelRatings',
     description: 'LabelRatings\' wrapper has flexDirection: row',
     render: () => (
       <View style={{ flex: 1, flexDirection: 'row' }}>
-        <LabelRating title="WS" rating="92" />
+        <LabelRating title="SZ" rating="75" />
         <LabelRating title="RP" rating="88" style={{ backgroundColor: ColorPallete.GREEN }} />
         <LabelRating title="SK" rating="72" style={{ backgroundColor: ColorPallete.BROWN }} />
         <LabelRating title="OE" rating="60" style={{ backgroundColor: ColorPallete.YELLOW }} />
