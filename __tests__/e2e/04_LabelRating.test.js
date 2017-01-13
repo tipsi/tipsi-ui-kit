a681b4b8437cd7f4991b84a6795ec83c5d41fd3d
Dmytro Zavgorodniy <dmytro.zavgorodniy@gmail.com>
2017-01-13 17:54:11 +0200

    Update e2e tests

--- __tests__/e2e/04_LabelRating.test.js
+++ __tests__/e2e/04_LabelRating.test.js
@@ -6,13 +6,30 @@ const { driver, select, idFromXPath } = helper
 test('<LabelRating />', async (t) => {
   const groupId = select({
     ios: idFromXPath(`//
-      XCUIElementTypeScrollView/XCUIElementTypeOther/XCUIElementTypeOther
+      XCUIElementTypeScrollView/XCUIElementTypeOther
     `),
     android: idFromXPath(`//
-      android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/
-      android.widget.FrameLayout[1]/android.view.View[1]/android.view.View[2]/
-      android.view.View[1]/android.widget.ScrollView[1]/android.view.View[1]/
-      android.view.View
+      android.widget.ScrollView[1]/android.view.View[1]
+    `),
+  })
+  const titleTextId = select({
+    ios: idFromXPath(`
+      ${groupId}/XCUIElementTypeOther[labelRatingId]/XCUIElementTypeOther[2]/
+      XCUIElementTypeOther/XCUIElementTypeOther[1]/XCUIElementTypeStaticText
+    `),
+    android: idFromXPath(`
+      ${groupId}/android.view.View[labelRatingId]/android.view.View[2]/
+      android.view.View[1]/android.widget.TextView[1]
+    `),
+  })
+  const ratingTextId = select({
+    ios: idFromXPath(`
+      ${groupId}/XCUIElementTypeOther[labelRatingId]/XCUIElementTypeOther[2]/
+      XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeStaticText
+    `),
+    android: idFromXPath(`
+      ${groupId}/android.view.View[labelRatingId]/android.view.View[2]/
+      android.view.View[2]/android.widget.TextView[1]
     `),
   })
 
@@ -27,29 +44,21 @@ test('<LabelRating />', async (t) => {
     await helper.openExampleFor('<LabelRating />')
 
     for (const model of models) {
-      const id = `${groupId}[${model.id}]`
+      const currentTitleTextId = titleTextId.replace('labelRatingId', model.id)
 
-      const currentTitleId = select({
-        ios: `${id}/XCUIElementTypeOther[2]/XCUIElementTypeOther/XCUIElementTypeOther[1]/XCUIElementTypeStaticText`,
-        android: `${id}/android.view.View[2]/android.view.View[1]/android.widget.TextView[1]`,
-      })
-
-      await driver.waitForVisible(currentTitleId, 20000)
+      await driver.waitForVisible(currentTitleTextId, 20000)
       t.pass(`LabelRating (id: ${model.id}) title text should be visible`)
 
-      const title = await driver.getText(currentTitleId)
+      const title = await driver.getText(currentTitleTextId)
       t.equal(title, model.title, `LabelRating (id: ${model.id}) should has title: ${model.title}`)
 
-      const currentRatingId = select({
-        ios: `${id}/XCUIElementTypeOther[2]/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeStaticText`,
-        android: `${id}/android.view.View[2]/android.view.View[2]/android.widget.TextView[1]`,
-      })
+      const currentRatingTextId = ratingTextId.replace('labelRatingId', model.id)
 
-      await driver.waitForVisible(currentRatingId, 20000)
+      await driver.waitForVisible(currentRatingTextId, 20000)
       t.pass(`LabelRating (id: ${model.id}) rating text should be visible`)
 
       const expectedRating = model.rating || defaultRating
-      const rating = await driver.getText(currentRatingId)
+      const rating = await driver.getText(currentRatingTextId)
       t.equal(rating, expectedRating, `LabelRating (id: ${model.id}) should has rating: ${expectedRating}`)
     }
 
