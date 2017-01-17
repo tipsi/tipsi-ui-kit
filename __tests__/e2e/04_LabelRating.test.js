db65e42d8af05ad5ae22f6e93bdc07df4dc2eebd
Dmytro Zavgorodniy <dmytro.zavgorodniy@gmail.com>
2017-01-17 22:05:35 +0800

    Update e2e tests

--- __tests__/e2e/04_LabelRating.test.js
+++ __tests__/e2e/04_LabelRating.test.js
@@ -6,60 +6,112 @@ const { driver, select, idFromXPath } = helper
 test('<LabelRating />', async (t) => {
   const groupId = select({
     ios: idFromXPath(`//
-      XCUIElementTypeScrollView/XCUIElementTypeOther
+      XCUIElementTypeScrollView/XCUIElementTypeOther/XCUIElementTypeOther[groupId]/XCUIElementTypeOther[2]
     `),
     android: idFromXPath(`//
-      android.widget.ScrollView[1]/android.view.View[1]
+      android.widget.ScrollView[1]/android.view.View[1]/android.view.View[groupId]
+    `),
+  })
+  const containerId = select({
+    ios: idFromXPath(`
+      XCUIElementTypeOther
+    `),
+    android: idFromXPath(`
+      android.view.View[1]
+    `),
+  })
+  const labelRatingId = select({
+    ios: idFromXPath(`
+      XCUIElementTypeOther[labelRatingId]
+    `),
+    android: idFromXPath(`
+      android.view.View[labelRatingId]
     `),
   })
   const titleTextId = select({
     ios: idFromXPath(`
-      ${groupId}/XCUIElementTypeOther[labelRatingId]/XCUIElementTypeOther[2]/
-      XCUIElementTypeOther/XCUIElementTypeOther[1]/XCUIElementTypeStaticText
+      XCUIElementTypeOther[1]/XCUIElementTypeStaticText
     `),
     android: idFromXPath(`
-      ${groupId}/android.view.View[labelRatingId]/android.view.View[2]/
       android.view.View[1]/android.widget.TextView[1]
     `),
   })
   const ratingTextId = select({
     ios: idFromXPath(`
-      ${groupId}/XCUIElementTypeOther[labelRatingId]/XCUIElementTypeOther[2]/
-      XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeStaticText
+      XCUIElementTypeOther[2]/XCUIElementTypeStaticText
     `),
     android: idFromXPath(`
-      ${groupId}/android.view.View[labelRatingId]/android.view.View[2]/
       android.view.View[2]/android.widget.TextView[1]
     `),
   })
 
-  const defaultRating = '0'
-  const models = [
-    { id: 1, title: 'WS', rating: '92' },
-    { id: 2, title: 'LR' },
-    { id: 3, title: 'NT', rating: '22' },
-  ]
+  const groups = []
+
+  // build "Title And Rating" group
+  const titleAndRatingGroup = {
+    labelRatingsParentId: `${groupId}`.replace('groupId', 1),
+    labelRatings: [
+      { title: 'EXAMPLE', rating: '15' },
+    ],
+  }
+  groups.push(titleAndRatingGroup)
+
+  // build "Types" group
+  const typesGroup = {
+    labelRatingsParentId: select({
+      ios: `${groupId}/${containerId}`,
+      android: `${groupId}`,
+    }).replace('groupId', 2),
+    labelRatings: [
+      { title: 'PR', rating: '88' },
+      { title: 'SU', rating: '72' },
+      { title: 'WA', rating: '60' },
+      { title: 'AL', rating: '92' },
+    ],
+  }
+  groups.push(typesGroup)
+
+  // build "Custom Theme" group
+  const customThemeGroup = {
+    labelRatingsParentId: `${groupId}`.replace('groupId', 3),
+    labelRatings: [
+      { title: 'SIZE', rating: '30' },
+    ],
+  }
+  groups.push(customThemeGroup)
 
   try {
     await helper.openExampleFor('<LabelRating />')
 
-    for (const model of models) {
-      const currentTitleTextId = titleTextId.replace('labelRatingId', model.id)
+    for (const group of groups) {
+      const currentLabelRatingsParentId = group.labelRatingsParentId
+      const currentLabelRatings = group.labelRatings
 
-      await driver.waitForVisible(currentTitleTextId, 20000)
-      t.pass(`LabelRating (id: ${model.id}) title text should be visible`)
 
-      const title = await driver.getText(currentTitleTextId)
-      t.equal(title, model.title, `LabelRating (id: ${model.id}) should has title: ${model.title}`)
+      for (const labelRating of currentLabelRatings) {
+        const index = currentLabelRatings.indexOf(labelRating)
+        const indexShift = select({
+          ios: 1,
+          android: 2,
+        })
 
-      const currentRatingTextId = ratingTextId.replace('labelRatingId', model.id)
+        const currentLabelRatingId = `${currentLabelRatingsParentId}/${labelRatingId}`.replace('labelRatingId', (index + indexShift))
 
-      await driver.waitForVisible(currentRatingTextId, 20000)
-      t.pass(`LabelRating (id: ${model.id}) rating text should be visible`)
+        const currentLabelRatingDescription = `[${labelRating.title}-${labelRating.rating}]`
 
-      const expectedRating = model.rating || defaultRating
-      const rating = await driver.getText(currentRatingTextId)
-      t.equal(rating, expectedRating, `LabelRating (id: ${model.id}) should has rating: ${expectedRating}`)
+        await driver.waitForVisible(currentLabelRatingId, 20000)
+        t.pass(`${currentLabelRatingDescription} should be visible`)
+
+        const titleText = labelRating.title
+        const currentTitleTextId = `${currentLabelRatingId}/${titleTextId}`
+        const sutCurrentTitleText = await driver.getText(currentTitleTextId)
+        t.equal(sutCurrentTitleText, titleText, `${currentLabelRatingDescription} should has title: ${titleText}`)
+
+        const ratingText = labelRating.rating
+        const currentRatingTextId = `${currentLabelRatingId}/${ratingTextId}`
+        const sutCurrentRatingText = await driver.getText(currentRatingTextId)
+        t.equal(sutCurrentRatingText, ratingText, `${currentLabelRatingDescription} should has rating: ${ratingText}`)
+      }
     }
 
     t.pass('<LabelRating /> example should be visible')
