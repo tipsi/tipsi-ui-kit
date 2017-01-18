14e63ef02b613895c7270b33559d1f5be3ded44f
dmitriy <dmitriy.malec@gmail.com>
2017-01-18 17:08:29 +0200

    - updated example

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes, Children, cloneElement } from 'react'
+import React, { Component, PropTypes } from 'react'
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
 export default class Counter extends Component {
@@ -6,11 +6,6 @@ export default class Counter extends Component {
     startValue: PropTypes.number,
     step: PropTypes.number,
     onValueChange: PropTypes.func,
-    // title: PropTypes.string,
-    // children: PropTypes.node,
-    // theme: PropTypes.oneOf(['light', 'dark']),
-    // style: StylePropType,
-    // titleStyle: StylePropType,
   }
 
   static defaultProps = {
@@ -22,18 +17,18 @@ export default class Counter extends Component {
   state = { count: this.props.startValue }
 
   onPressPlus = () => {
-    this.setState({ count: this.state.count + this.props.step })
-    this.props.onValueChange(this.state.count)
+    const newCount = this.state.count + this.props.step
+    this.setState({ count: newCount })
+    this.props.onValueChange(newCount)
   }
 
   onPressMinus = () => {
-    this.setState({ count: this.state.count - this.props.step })
-    this.props.onValueChange(this.state.count)
+    const newCount = this.state.count - this.props.step
+    this.setState({ count: newCount })
+    this.props.onValueChange(newCount)
   }
 
   render() {
-    // const { title, children, theme, style, titleStyle } = this.props
-
     return (
       <View style={styles.container}>
         <TouchableOpacity onPress={this.onPressMinus}>
@@ -58,30 +53,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'row',
-    // justifyContent: 'space-around',
-    // alignItems: 'center',
     justifyContent: 'center',
     alignItems: 'center',
   },
   item: {
     width: 40,
     height: 40,
-    // padding: 10,
     fontSize: 20,
     borderWidth: 2,
     borderColor: 'gainsboro',
     textAlignVertical: 'center',
-    // justifyContent: 'space-around',
-    // alignItems: 'center',
     textAlign: 'center',
   },
   center: {
     width: 80,
     borderLeftWidth: 0,
     borderRightWidth: 0,
-    // justifyContent: 'space-around',
-    // alignItems: 'center',
-    // textAlign: 'center',
   },
   left: {
     fontSize: 30,
@@ -94,20 +81,5 @@ const styles = StyleSheet.create({
     borderBottomRightRadius: 3,
     borderTopRightRadius: 3,
     backgroundColor: 'aliceblue',
-  },
-  title: {
-    marginLeft: 3,
-    marginBottom: 13,
-    fontSize: 15,
-    fontWeight: '600',
-  },
-  titleDark: {
-    color: 'white',
-  },
-  children: {
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    alignItems: 'flex-start',
   },
 })
--- uiexplorer/examples/Counter.js
+++ uiexplorer/examples/Counter.js
@@ -7,16 +7,22 @@ register.addExample({
   title: '<Counter />',
   description: 'Counter component',
   examples: [{
-  //   title: 'Simple example',
-  //   description: 'Props: onPress',
-  //   render: ({ action }) => ( // eslint-disable-line react/prop-types
-  //     <Counter onValueChange={action('Example onPress id = ')} />
-  //   ),
-  // }, {
     title: 'Default',
     description: 'Props: without props',
     render: () => (
       <Counter />
+      ),
+  }, {
+    title: 'Start values',
+    description: 'Props: step, startValue',
+    render: () => (
+      <Counter step={5} startValue={25} />
+      ),
+  }, {
+    title: 'Simple example',
+    description: 'Props: onValueChange',
+    render: ({ action }) => ( // eslint-disable-line react/prop-types
+      <Counter onValueChange={action('current value')} />
       ),
   }],
 })
