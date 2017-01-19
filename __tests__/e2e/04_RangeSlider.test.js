48cc04e780d1abe6478b803d1cc9df5fe7424f91
dmitriy <dmitriy.malec@gmail.com>
2017-01-20 00:20:35 +0200

    - change e2e test, added ability move slider

--- __tests__/e2e/04_RangeSlider.test.js
+++ __tests__/e2e/04_RangeSlider.test.js
@@ -1,54 +1,54 @@
 import test from 'tape-async'
 import helper from 'tipsi-appium-helper'
 
-const { driver, idFromXPath, select } = helper
+const { driver, idFromXPath, select, platform } = helper
 
 test('<RangeSlider />', async (t) => {
-
   const sliderGroupId = select({
     ios: idFromXPath(`//
       XCUIElementTypeScrollView[1]/*/*/XCUIElementTypeOther[2]/
-      XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[2]
+      XCUIElementTypeOther[1]/XCUIElementTypeOther[1]
     `),
     android: idFromXPath(`//
       android.widget.ScrollView[1]/android.view.View[1]/android.view.View[1]
     `),
   })
 
-  //android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.view.View[1]/android.view.View[2]/android.view.View[1]/android.widget.ScrollView[1]/android.view.View[1]/android.view.View[1]/android.view.View[3]
-  //android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.view.View[1]/android.view.View[2]/android.view.View[1]/android.widget.ScrollView[1]/android.view.View[1]/android.view.View[1]/android.view.View[5]
-
-  //android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.view.View[1]/android.view.View[2]/android.view.View[1]/android.widget.ScrollView[1]/android.view.View[1]/android.view.View[1]/android.view.View[4]
-
-  //XCUIElementTypeApplication[1]/XCUIElementTypeWindow[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[2]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeScrollView[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[2]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[2]/XCUIElementTypeOther[1]
-  //XCUIElementTypeApplication[1]/XCUIElementTypeWindow[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[2]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeScrollView[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[2]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]
-
-  //XCUIElementTypeApplication[1]/XCUIElementTypeWindow[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[2]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeScrollView[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[2]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[2]/XCUIElementTypeOther[4]
-
   const firstHandle = select({
     ios: idFromXPath(`
-      ${sliderGroupId}/XCUIElementTypeOther[1]
+      ${sliderGroupId}/XCUIElementTypeOther[2]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]
     `),
     android: idFromXPath(`
-      ${sliderGroupId}/android.view.View[3]
+      ${sliderGroupId}/android.view.View[3]/android.view.View[1]/
+      android.view.View[1]/android.view.View[1]
     `),
   })
 
   const secondHandle = select({
     ios: idFromXPath(`
-      ${sliderGroupId}/XCUIElementTypeOther[2]
+      ${sliderGroupId}/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]
     `),
     android: idFromXPath(`
-      ${sliderGroupId}/android.view.View[5]
+      ${sliderGroupId}/android.view.View[5]/android.view.View[1]/
+      android.view.View[1]/android.view.View[1]
     `),
   })
 
   const track = select({
     ios: idFromXPath(`
-      ${sliderGroupId}/XCUIElementTypeOther[4]
+      ${sliderGroupId}//XCUIElementTypeOther[2]XCUIElementTypeOther[4]
     `),
     android: idFromXPath(`
       ${sliderGroupId}/android.view.View[4]
+    `),
+  })
+
+  const minValue = select({
+    ios: idFromXPath(`
+      ${sliderGroupId}/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeStaticText[1]
+    `),
+    android: idFromXPath(`
+      ${sliderGroupId}/android.widget.TextView[1]
     `),
   })
 
@@ -63,6 +63,39 @@ test('<RangeSlider />', async (t) => {
 
     await driver.waitForVisible(track, 10000)
     t.pass('track should be visible')
+
+    await driver.waitForVisible(minValue, 10000)
+    t.pass('minValue should be visible')
+
+    const minValueText = await driver.getText(minValue)
+    t.equal(minValueText, '2', 'minValue should be 2')
+
+
+    if (platform('android')) {
+      const element = await driver.element(firstHandle)
+      await driver.touchPerform([{
+        action: 'press',
+        options: {
+          element: element.value.ELEMENT,
+        },
+      }, {
+        action: 'moveTo',
+        options: {
+          x: -100,
+          y: 0,
+        },
+      }, {
+        action: 'release',
+      }])
+      t.pass('firstHandle was moved')
+
+      // await driver.waitUntil(() => (
+      //   driver.getText(minValue) === '0'
+      // ), 3000, 'minValue still not 0')
+      //
+      // const minValueText2 = await driver.getText(minValue)
+      // t.equal(minValueText2, '0', 'Now minValue should be 0')
+    }
 
     t.pass('<RangeSlider /> example should be visible')
   } catch (error) {
