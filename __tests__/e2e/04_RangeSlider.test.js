e1d6dc51ee5537a27672b88e767f52447ed7c61b
Anton Petrov <petrov.aap@gmail.com>
2017-01-20 18:01:10 +0700

    Updated e2e tests

--- __tests__/e2e/04_RangeSlider.test.js
+++ __tests__/e2e/04_RangeSlider.test.js
@@ -4,100 +4,83 @@ import helper from 'tipsi-appium-helper'
 const { driver, idFromXPath, select, platform } = helper
 
 test('<RangeSlider />', async (t) => {
-  const sliderGroupId = select({
+  const sliderId = select({
     ios: idFromXPath(`//
-      XCUIElementTypeScrollView[1]/*/*/XCUIElementTypeOther[2]/
-      XCUIElementTypeOther[1]/XCUIElementTypeOther[1]
+      XCUIElementTypeScrollView[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/
+      XCUIElementTypeOther[2]/XCUIElementTypeOther[1]
     `),
     android: idFromXPath(`//
-      android.widget.ScrollView[1]/android.view.View[1]/android.view.View[1]
+      android.widget.ScrollView[1]/android.view.ViewGroup[1]/android.view.ViewGroup[1]
     `),
   })
 
-  const firstHandle = select({
+  const firstMarkerId = select({
     ios: idFromXPath(`
-      ${sliderGroupId}/XCUIElementTypeOther[2]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]
+      ${sliderId}/XCUIElementTypeOther[1]/XCUIElementTypeOther[2]/
+      XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]
     `),
     android: idFromXPath(`
-      ${sliderGroupId}/android.view.View[3]/android.view.View[1]/
-      android.view.View[1]/android.view.View[1]
+      ${sliderId}/android.view.ViewGroup[3]/android.view.ViewGroup[1]/
+      android.view.ViewGroup[1]/android.view.ViewGroup[1]
     `),
   })
 
-  const secondHandle = select({
+  const secondMarkerId = select({
     ios: idFromXPath(`
-      ${sliderGroupId}/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]
+      ${sliderId}/XCUIElementTypeOther[1]/XCUIElementTypeOther[2]/
+      XCUIElementTypeOther[2]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]
     `),
     android: idFromXPath(`
-      ${sliderGroupId}/android.view.View[5]/android.view.View[1]/
-      android.view.View[1]/android.view.View[1]
+      ${sliderId}/android.view.ViewGroup[5]/android.view.ViewGroup[1]/
+      android.view.ViewGroup[1]/android.view.ViewGroup[1]
     `),
   })
 
-  const track = select({
+  const minValueId = select({
     ios: idFromXPath(`
-      ${sliderGroupId}//XCUIElementTypeOther[2]XCUIElementTypeOther[4]
+      ${sliderId}/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/
+      XCUIElementTypeOther[1]/XCUIElementTypeStaticText[1]
     `),
     android: idFromXPath(`
-      ${sliderGroupId}/android.view.View[4]
-    `),
-  })
-
-  const minValue = select({
-    ios: idFromXPath(`
-      ${sliderGroupId}/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeStaticText[1]
-    `),
-    android: idFromXPath(`
-      ${sliderGroupId}/android.widget.TextView[1]
+      ${sliderId}/android.widget.TextView[1]
     `),
   })
 
   try {
     await helper.openExampleFor('<RangeSlider />')
 
-    await driver.waitForVisible(firstHandle, 20000)
-    t.pass('first handle should be visible')
+    await driver.waitForVisible(sliderId, 20000)
+    t.pass('slider should be visible')
 
-    await driver.waitForVisible(secondHandle, 10000)
-    t.pass('second handle should be visible')
+    await driver.waitForVisible(firstMarkerId, 10000)
+    t.pass('first marker should be visible')
 
-    await driver.waitForVisible(track, 10000)
-    t.pass('track should be visible')
+    await driver.waitForVisible(secondMarkerId, 10000)
+    t.pass('second marker should be visible')
 
-    await driver.waitForVisible(minValue, 10000)
-    t.pass('minValue should be visible')
+    await driver.waitForVisible(minValueId, 10000)
+    t.pass('min value should be visible')
 
-    const minValueText = await driver.getText(minValue)
-    t.equal(minValueText, '2', 'minValue should be 2')
+    const minValueText = await driver.getText(minValueId)
+    t.equal(minValueText, '2', 'min value should be 2')
 
-
-    if (platform('android')) {
-      const element = await driver.element(firstHandle)
+    if (platform('ios')) {
+      const element = await driver.element(firstMarkerId)
       await driver.touchPerform([{
         action: 'press',
-        options: {
-          element: element.value.ELEMENT,
-        },
+        options: { element: element.value.ELEMENT },
       }, {
         action: 'moveTo',
-        options: {
-          x: -100,
-          y: 0,
-        },
+        options: { x: -200, y: 0 },
       }, {
         action: 'release',
       }])
-      t.pass('firstHandle was moved')
+    } else (
+      await driver.swipeLeft(firstMarkerId, 200)
+    )
 
-      // await driver.waitUntil(() => (
-      //   driver.getText(minValue) === '0'
-      // ), 3000, 'minValue still not 0')
-      //
-      // const minValueText2 = await driver.getText(minValue)
-      // t.equal(minValueText2, '0', 'Now minValue should be 0')
-    }
-
-    t.pass('<RangeSlider /> example should be visible')
+    const minValueTextAfterMove = await driver.getText(minValueId)
+    t.equal(minValueTextAfterMove, '0', 'markers should be moveable')
   } catch (error) {
     await helper.screenshot()
     await helper.source()
