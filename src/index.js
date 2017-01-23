26c7f2eef9dd32adb0b92aba07b7be8cde4d1c9f
Anton Petrov <petrov.aap@gmail.com>
2017-01-23 15:16:16 +0700

    Fixed imports

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
 export { default as StarRating } from './StarRating'
 export { default as Tags } from './Tags'
 export { default as Breadcrumbs } from './Breadcrumbs'
+export { default as Dash } from './Dash'
 export { default as Carousel } from './Carousel'
 export { default as Label } from './Label'
 export { default as LabelRating } from './LabelRating'
